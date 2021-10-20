94fdc4be96185e5af35bc0b0b08dfa512be1563b
ambika10 <71698170+ambika10@users.noreply.github.com>
2021-10-20 15:57:56 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//https://teachablemachine.withgoogle.com/models/xRyQQFKq2/
+Webcam.set({
+    width: 300, 
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"></img>';
+    })
+}
+
+console.log("ml5 version", ml5.version);
